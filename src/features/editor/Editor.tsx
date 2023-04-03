c00edeca3582bdda85ab4ae2ada65ec27f69429b
pinvokeee <nanasisine+1@gmail.com>
2023-04-03 09:07:07 +0900

    fix

--- src/features/editor/Editor.tsx
+++ src/features/editor/Editor.tsx
@@ -9,6 +9,7 @@ import { JobSelecter } from "./components/JobSelecter";
 import { MultiLineField } from "./components/MultiLineField";
 import { jobCollectionState } from "../../state/jobCollectionState";
 import { Fields } from "./components/Fields";
+import { FormInputData } from "../../types/EditDataObject";
 
 const Container = styled(Stack)(({theme}) =>
 (
@@ -21,7 +22,7 @@ export const Editor = () =>
 {
     const state = editDataState.useEditDataObject();
     const actions = editDataState.useActions();
-    
+
     const forms = editDataState.useCurrentFormData();
 
     console.log(forms);
@@ -35,6 +36,10 @@ export const Editor = () =>
     const handleJobChange = useCallback((name: string, value: string) => {
         actions.setValue(name, value);
     }, [])
+    
+    const handleFormsChange = useCallback((value: FormInputData[]) => {
+        actions.setValue(state.subJob, value);
+    }, [])
 
     return (
         <Container gap={2}>
@@ -45,10 +50,10 @@ export const Editor = () =>
                 subSelectedId={state.subJob} 
                 onChange={handleJobChange} />
 
-        <Fields forms={forms} />
+            <Fields forms={forms} onChange={handleFormsChange} />
 
-        <MultiLineField text="5.退職理由" name="freeText" value={state.freeText} onChange={handleChange}></MultiLineField>
-        <MultiLineField text="5.退職理由" name="freeText" value={state.freeText} onChange={handleChange}></MultiLineField>
+            <MultiLineField text="5.退職理由" name="freeText" value={state.freeText} onChange={handleChange}></MultiLineField>
+            <MultiLineField text="5.退職理由" name="freeText" value={state.freeText} onChange={handleChange}></MultiLineField>
         </Container>
         
     )
